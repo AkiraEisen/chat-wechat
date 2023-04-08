cab45a364aac86b85b583f37987ba05ecd7d7471
heyongquan <akira.eisen@gmail.com>
2023-04-08 17:21:47 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { init: initDB, Counter } = require("./db");
+var xml2js = require("xml2js");
+//创建xml->js的对象
+var parser = new xml2js.Parser({explicitArray: false});
+//创建js->xml的对象
+var builder = new xml2js.Builder({rootName: "xml", cdata: true, headless: true});
+
 
 const logger = morgan("tiny");
 
@@ -12,46 +18,80 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+//3. 设置路由
+app.get("/", function(req, res){
+  // console.log("有人来请求了！！");
+  //当配置了微信公众号的服务器URL之后，微信服务器会向当前地址发送一个校验请求
+  //校验请求是一个get请求，参数中携带了一个echostr
+  //我们需要将这个echostr原样返回给微信服务器，返回成功之后，校验成功，开发者配置生效
 
-app.post("/api/entry", async (req, res) => {
-  console.log(req)
+  //1. express中获取get请求的参数
+  console.log(req.query);
+
+  //2. 将请求参数中的echostr原样返回给 微信服务器即可通过验证
+  res.send(req.query.echostr);
 })
 
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
+//添加post的路由，处理微信服务器转发过来的用户的消息
+app.post("/", function(req, res){
+  // console.log("用户发送消息了");
+  //1. 获取post请求中的内容
 
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
+  var bufferList = [];
+  req.on("data", function(chunk){
+    bufferList.push(chunk);
+  })
 
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
-});
+  req.on("end", function(){
+    var result = Buffer.concat(bufferList);
+    //将获取到的微信服务器发来的消息数据 使用 xml2js转成js对象
+    parser.parseString(result.toString(), function(err, result){
+
+      //声明一个变量，用来保存我们最终要回复给用户的消息
+      var msg = "";
+
+      //如果是文本消息：就处理
+      if(result.xml.MsgType == "text"){
+        switch(result.xml.Content){
+          case "1":
+            msg = "你好，很高兴认识你";
+            break;
+          case "2":
+            msg = "Hello, Nice to meet you";
+            break;
+          case "3":
+            msg = "aniasiyo"
+            break;
+          default:
+            msg = "请选择您的语言：1. 汉语 2. 英语 3. 日语"
+        }
+      }
+
+      //将最终要回复给用户的消息，响应给微信服务器
+      //这个消息，也是以xml格式的数据进行发送的
+
+      //响应给微信服务器的数据中也包含如下几个参数：
+      //ToUserName: '哪个用户发消息来的',
+      //FromUserName: '写自己公众号的id',
+      //CreateTime: 消息的发送时间,
+      //MsgType: 'text',
+      //Content: '要回复给用户的消息'
+
+      //创建一个回复消息的对象
+      var returnMsg = {
+        ToUserName: result.xml.FromUserName,
+        FromUserName: result.xml.ToUserName,
+        CreateTime: +new Date(),
+        MsgType: "text",
+        Content: msg
+      }
+
+      //将回复消息对象转换成xml格式的数据，响应给微信服务器
+      res.send(builder.buildObject(returnMsg));
+
+    })
+  })
+})
 
 const port = process.env.PORT || 80;
 
